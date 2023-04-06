c42c013cdb23d7e2c3970876d5581d7258d04f66
camuSssssssssssssssssssssssssssssssssss <83710316+camuSssssssssssssssssssssssssssssssssss@users.noreply.github.com>
2023-04-06 20:49:18 +0400

    Update simple_vector.h

--- simple-vector/simple_vector.h
+++ simple-vector/simple_vector.h
@@ -1,12 +1,9 @@
 #pragma once
 #include <cassert>
+#include <iterator>
 #include <iostream>
-#include <numeric>
 #include <initializer_list>
-#include <memory>
-#include <stdexcept>
-
-using namespace std;
+#include "array_ptr.h"
 
 class ReserveProxyObj {
 public:
@@ -17,44 +14,37 @@ public:
 };
 
 template <typename Type>
-class SimpleVector
-{
+class SimpleVector {
 private:
     size_t size_ = 0;
     size_t capacity_ = 0;
-    std::unique_ptr<Type[]> array_;
+    ArrayPtr<Type> array_;
 
 public:
     using Iterator = Type*;
     using ConstIterator = const Type*;
 
-
     SimpleVector() noexcept = default;
 
     SimpleVector(ReserveProxyObj other) {
-        // ???????? ???? ???????????? ??????????????
         Reserve(other.capacity_to_reserve);
     }
 
-
     explicit SimpleVector(size_t size)
-        : size_(size), capacity_(size), array_(std::make_unique<Type[]>(capacity_))
-    {
+        : size_(size), capacity_(size), array_(size) {
     }
 
     SimpleVector(size_t size, const Type& value)
-        : size_(size), capacity_(size), array_(std::make_unique<Type[]>(capacity_)) {
+        : size_(size), capacity_(size), array_(size) {
         for (size_t i = 0; i < size_; ++i) {
             array_[i] = value;
         }
     }
 
     SimpleVector(std::initializer_list<Type> init)
-        : size_(init.size()), capacity_(init.size()), array_(std::make_unique<Type[]>(capacity_))
-    {
+        : size_(init.size()), capacity_(init.size()), array_(size_) {
         size_t i = 0;
-        for (const auto& item : init)
-        {
+        for (const auto& item : init) {
             array_[i] = item;
             ++i;
         }
@@ -73,10 +63,12 @@ public:
     }
 
     Type& operator[](size_t index) noexcept {
+        assert(index < size_);
         return array_[index];
     }
 
     const Type& operator[](size_t index) const noexcept {
+        assert(index < size_);
         return array_[index];
     }
 
@@ -100,11 +92,11 @@ public:
 
     void Resize(size_t new_size) {
         if (new_size > capacity_) {
-            auto new_array = std::make_unique<Type[]>(new_size);
+            auto new_array = ArrayPtr<Type>(new_size);
             for (size_t i = 0; i < size_; ++i) {
                 new_array[i] = std::move(array_[i]);
             }
-            array_ = std::move(new_array);
+            array_.swap(new_array);
             capacity_ = new_size;
         }
         for (size_t i = size_; i < new_size; ++i) {
@@ -114,33 +106,33 @@ public:
     }
 
     Iterator begin() noexcept {
-        return array_.get();
+        return array_.Get();
     }
 
     Iterator end() noexcept {
-        return array_.get() + size_;
+        return array_.Get() + size_;
     }
 
     ConstIterator begin() const noexcept {
-        return array_.get();
+        return array_.Get();
     }
 
     ConstIterator end() const noexcept {
-        return array_.get() + size_;
+        return array_.Get() + size_;
     }
 
     ConstIterator cbegin() const noexcept {
-        return array_.get();
+        return array_.Get();
     }
 
     ConstIterator cend() const noexcept {
-        return array_.get() + size_;
+        return array_.Get() + size_;
     }
 
     void swap(SimpleVector& other) noexcept {
         std::swap(size_, other.size_);
         std::swap(capacity_, other.capacity_);
-        std::swap(array_, other.array_);
+        array_.swap(other.array_);
     }
 
     Iterator Erase(ConstIterator pos) {
@@ -153,38 +145,40 @@ public:
     }
 
     void PopBack() noexcept {
-        --size_;
+        if (!IsEmpty()) {
+            --size_;
+        }
+        
     }
 
     Iterator Insert(ConstIterator pos, Type&& value) {
-        const size_t idx = pos - cbegin();
-        if (size_ == capacity_) {
-            capacity_ = capacity_ == 0 ? 1 : 2 * capacity_;
-            auto new_array = std::make_unique<Type[]>(capacity_);
-            for (size_t i = 0; i != idx; ++i) {
-                new_array[i] = std::move(array_[i]);
-            }
-            new_array[idx] = std::move(value);
-            for (size_t i = idx; i != size_; ++i) {
-                new_array[i + 1] = std::move(array_[i]);
-            }
-            array_ = std::move(new_array);
+        assert(pos >= begin() && pos <= end());
+        size_t idx = pos - array_.Get();
+
+        if (size_ >= capacity_) {
+            size_t new_capacity = capacity_ == 0 ? 1 : 2 * capacity_;
+
+            ArrayPtr<Type> new_data(new_capacity);
+            std::generate(new_data.Get(), new_data.Get() + new_capacity, []() { return Type(); });
+
+            std::move(array_.Get(), const_cast<Iterator>(pos), new_data.Get());
+            new_data[idx] = std::move(value);
+            std::move(const_cast<Iterator>(pos), array_.Get() + size_, new_data.Get() + idx + 1);
+
+            array_.swap(new_data);
+            capacity_ = new_capacity;
         }
         else {
-            for (size_t i = size_; i != idx; --i) {
-                array_[i] = std::move(array_[i - 1]);
-            }
+            std::move_backward(const_cast<Iterator>(pos), array_.Get() + size_, array_.Get() + size_ + 1);
             array_[idx] = std::move(value);
         }
         ++size_;
-        return begin() + idx;
+        return Iterator(array_.Get() + idx);
     }
-
 
     void PushBack(Type&& item) {
         Insert(end(), std::move(item));
     }
-
 
     SimpleVector& operator=(const SimpleVector& rhs) {
         if (&rhs == this)
@@ -197,32 +191,30 @@ public:
     SimpleVector(const SimpleVector& other)
         : size_(other.GetSize())
         , capacity_(other.GetCapacity())
-        , array_(std::make_unique<Type[]>(capacity_))
-    {
+        , array_(other.GetSize()) {
         std::copy(other.begin(), other.end(), begin());
     }
 
     void Reserve(size_t new_capacity) {
         if (new_capacity > capacity_) {
-            auto new_array = std::make_unique<Type[]>(new_capacity);
-            std::copy(begin(), end(), new_array.get());
-            array_ = std::move(new_array);
+            auto new_array = ArrayPtr<Type>(new_capacity);
+            std::copy(begin(), end(), new_array.Get());
+            array_.swap(new_array);
             capacity_ = new_capacity;
         }
     }
 
     SimpleVector(SimpleVector&& other) noexcept {
-        *this = std::move(other);
+        swap(other);
     }
 
-    SimpleVector& operator=(SimpleVector&& rhs) noexcept {
-        if (this != &rhs) {
-            size_ = rhs.size_;
-            capacity_ = rhs.capacity_;
-            array_ = std::move(rhs.array_);
-            rhs.size_ = 0;
-            rhs.capacity_ = 0;
-        }
+    SimpleVector& operator=(SimpleVector&& rhs) {
+        delete[] array_.Release();
+        ArrayPtr<Type> array_ptr(rhs.size_);
+        std::move(rhs.begin(), rhs.end(), array_ptr.Get());
+        array_.swap(array_ptr);
+        size_ = std::move(rhs.size_);
+        capacity_ = size_;
         return *this;
     }
 
